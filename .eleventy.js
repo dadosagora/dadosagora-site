6cdc694229eb7e01236eca27a190181e058d1655
dadosagora <dadosagora121@gmail.com>
2025-06-26 15:31:48 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,8 @@
-module.exports = function(eleventyConfig) {
-  return {
-    dir: {
-      input: "src",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
-  };
-};
+module.exports = () => ({
+  dir: {
+    input: "src",
+    includes: "_includes",
+    data: "_data",
+    output: "_site"
+  }
+});
