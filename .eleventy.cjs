c5ac63e4de6e63c694c2403c5397a0b055c47fc8
dadosagora <dadosagora121@gmail.com>
2025-06-26 15:19:39 -0300

    Update .eleventy.cjs

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,10 +1,10 @@
-module.exports = function (eleventyConfig) {
+module.exports = function(eleventyConfig) {
   return {
     dir: {
-      input: "src",        // todos os arquivos fonte estão em src/
-      includes: "_includes", // layouts ficam em src/_includes
-      data: "_data",         // dados (JSON, JS) em src/_data
-      output: "_site"        // pasta de saída
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
     }
   };
 };
