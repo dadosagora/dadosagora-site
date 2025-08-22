43449cbae90046f7befa198af8706bae2d5feafd
dadosagora <dadosagora121@gmail.com>
2025-08-22 18:17:28 -0300

    Update eleventy.js

--- eleventy.js
+++ eleventy.js
@@ -1,16 +1,16 @@
 module.exports = function (eleventyConfig) {
-  // Copiar assets da pasta src para a saída (_site)
+  // Copia os assets da pasta src para a saída
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
 
   return {
     dir: {
-      input: "src",        // onde estão seus .njk
+      input: "src",        // diz que seus .njk estão em src/
       includes: "_includes",
       data: "_data",
-      output: "_site"
+      output: "_site",
     },
     templateFormats: ["njk", "md", "html", "11ty.js"],
     markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk"
+    htmlTemplateEngine: "njk",
   };
 };
